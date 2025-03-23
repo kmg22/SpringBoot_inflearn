7a7d1e0430ce8ed06d91aa55d9aef659aa79aff8
kmg22 <ssukmg22@gmail.com>
2025-03-24 01:34:56 +0900

    회원 목록 조회

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -12,24 +12,26 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 import static org.apache.tomcat.util.net.openssl.OpenSSLStatus.getName;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
-    
+
     private final MemberService memberService;
 
     @GetMapping("/members/new")
-    public String createForm(Model model){
+    public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
     }
 
     @PostMapping("/members/new")
-    public String create(@Valid MemberForm form, BindingResult result){
+    public String create(@Valid MemberForm form, BindingResult result) {
 
-        if(result.hasErrors()){
+        if (result.hasErrors()) {
             return "members/createMemberForm";
         }
 
@@ -41,6 +43,12 @@ public class MemberController {
 
         memberService.join(member); // 저장
         return "redirect:/";
+    }
 
+    @GetMapping("/members")
+    public String list(Model model) { // 실무에서는 MemeberForm으로 출력하는 것이 좋음
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
